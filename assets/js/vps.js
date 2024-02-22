9e20d650597d290f2e4c5006373c24f4a1928ade
Fayzaaup <139423061+Fayzaaup@users.noreply.github.com>
2024-02-23 02:22:53 +0700

    Update vps.js

--- assets/js/vps.js
+++ assets/js/vps.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const panelName = this.parentElement.parentElement.querySelector('.info h5').innerText;
             const panelPrice = this.parentElement.parentElement.querySelector('.btc p').innerText;
 
-            const whatsappMessage = `𝗡𝗘𝗪 𝗢𝗥𝗗𝗘𝗥\n\n𝗣𝗥𝗢𝗗𝗨𝗞 : ${panelName}\n𝗣𝗥𝗜𝗖𝗘 : ${panelPrice}\n𝗣𝗔𝗬𝗠𝗘𝗡𝗧 : DANA,GOPAY,QRIS\n𝗜𝗦𝗣 : LINODE, DIGITAL OCEAL\n\n𝗡𝗼𝘁𝗲: Sertakan bukti transfer agar admin dapat memproses pesanan anda.`;
+            const whatsappMessage = `𝗡𝗘𝗪 𝗢𝗥𝗗𝗘𝗥\n\n𝗣𝗥𝗢𝗗𝗨𝗞 : ${panelName}\n𝗣𝗥𝗜𝗖𝗘 : ${panelPrice}\n𝗣𝗔𝗬𝗠𝗘𝗡𝗧 : DANA,GOPAY,QRIS\n𝗜𝗦𝗣 : LINODE, DIGITAL OCEAN\n\n𝗡𝗼𝘁𝗲: Sertakan bukti transfer agar admin dapat memproses pesanan anda.`;
 
             const whatsappLink = `https://wa.me/6285725849489?text=${encodeURIComponent(whatsappMessage)}`;
 
